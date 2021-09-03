38410231af2440ccfca1b977b54b41d4cff32fe7
ZhangYaqing <zhangyaqing59@126.com>
2021-09-03 14:17:35 +0800

    增加 health

--- main.go
+++ main.go
@@ -10,28 +10,98 @@ import (
 )
 
 func main() {
+	health()
+	system()
 
-	go func() {
-		for true {
-			time.Sleep(300 * time.Second)
+	//go func() {
+	//	//time.Sleep(60*time.Second)
+	//	health()
+	//}()
+	//
+	//go func() {
+	//	//time.Sleep(60*time.Second)
+	//
+	//}()
+	//time.Sleep(15*time.Second)
 
+}
+
+func health() {
+
+	mailTo := []string{
+		"zhangyaqing59@126.com",
+		//"30960425@qq.com",
+	}
+
+	//service : port
+	service := map[string]int{
+		"goods":     10050,
+		"message":   10040,
+		"cms":       10010,
+		"container": 10030,
+		"gateway":   10000,
+		"user":      10020,
+		"admin":     10001,
+	}
+
+	status := map[string]string{
+		"goods":     "OK",
+		"message":   "OK",
+		"cms":       "OK",
+		"container": "OK",
+		"gateway":   "OK",
+		"user":      "OK",
+		"admin":     "OK",
+	}
+
+	var t string
+	//测试服务是否正常，把不正常的服务记录
+	for k, v := range service {
+		//组装uri
+		uri := "http://127.0.0.1:" + string(v) + "/test/check"
+		// 测试uri
+		t = time.Now().Format("2006-01-02 15:04:05")
+		res := mod.HealthCheck(uri)
+		if res == "TimeOut" {
+
+			status[k] = "Failed"
 		}
-	}()
+	}
 
+	subject := "SERVICE INFO"
+	body := "<html>\n\n<body>\n<h4>Service status: </h4>\n" +
+		"<table border=\"1\">\n" +
+		"<tr>\n  <td>Service</td>\n  <td>Port</td>\n  <td>Status</td>\n  <td>Time</td>\n  <td>备注</td>\n</tr>\n" +
+		"<tr>\n  <td>goods</td>\n  <td>10050</td>\n  <td>" + status["goods"] + "</td>\n  <td>" + t + "</td>\n  <td>600</td>\n</tr>\n" +
+		"<tr>\n  <td>message</td>\n  <td>10040</td>\n  <td>" + status["message"] + "</td>\n  <td>" + t + "</td>\n  <td>600</td>\n</tr>\n" +
+		"<tr>\n  <td>cms</td>\n  <td>10010</td>\n  <td>" + status["cms"] + "</td>\n  <td>" + t + "</td>\n  <td>600</td>\n</tr>\n" +
+		"<tr>\n  <td>container</td>\n  <td>10030</td>\n  <td>" + status["container"] + "</td>\n  <td>" + t + "</td>\n  <td>600</td>\n</tr>\n" +
+		"<tr>\n  <td>gateway</td>\n  <td>10000</td>\n  <td>" + status["gateway"] + "</td>\n  <td>" + t + "</td>\n  <td>600</td>\n</tr>\n" +
+		"<tr>\n  <td>user</td>\n  <td>10020</td>\n  <td>" + status["user"] + "</td>\n  <td>" + t + "</td>\n  <td>600</td>\n</tr>\n" +
+		"<tr>\n  <td>admin</td>\n  <td>10001</td>\n  <td>" + status["admin"] + "</td>\n  <td>" + t + "</td>\n  <td>600</td>\n</tr>\n" +
+		"</table>\n\n</body>\n</html>\n"
+
+	err := email.SendMail(mailTo, subject, body)
+	if err != nil {
+		fmt.Println("email send failed ,", err)
+	}
+
+}
+
+func system() {
 	hosts := mod.Monitor{}
-
 	cpuinfo := hosts.Cpu.Cpuinfo()
 	diskinfo := hosts.Disks.Diskinfo("/data")
 	memroy := hosts.Memroy.Memoryinfo()
 
-	//定义收件人
 	mailTo := []string{
 		"zhangyaqing59@126.com",
+		//"30960425@qq.com",
 	}
 	//邮件主题为"Hello"
-	subject := "Hello"
+	info_subject := "SYSTEM INFO"
 	// 邮件正文
-	body := "<html>\n" +
+	info_body := "<html>\n" +
 		"<body>\n" +
 		"<h4>CPU INFO：</h4>\n<table border=\"1\">\n<tr>\n  <td>Cores</td>\n  <td>" + cpuinfo.CoreNmub + "</td>\n</tr>\n" +
 		"<tr>\n  <td>UseAge</td>\n  <td>" + cpuinfo.TotalPercent + "</td>\n</tr>\n</table>\n\n" +
@@ -50,7 +120,7 @@ func main() {
 		"</body>\n" +
 		"</html>\n"
 
-	err := email.SendMail(mailTo, subject, body)
+	err := email.SendMail(mailTo, info_subject, info_body)
 	if err != nil {
 		fmt.Println("email send failed ,", err)
 	}
--- mod/healthcheck.go
+++ mod/healthcheck.go
@@ -1,26 +1,35 @@
 package mod
 
 import (
-	"fmt"
 	"github.com/idoubi/goz"
+	"time"
 )
 
-func HealthCheck() {
-
-	service :=map[string]int{
-		"goods":
-	}
-
+func HealthCheck(uri string) (s string) {
+	s = "OK"
+	// create a curl instance
 	cli := goz.NewClient(goz.Options{
-		Timeout: 0.9,
+		Timeout: 0.5,
 	})
-	resp, err := cli.Get("http://127.0.0.1:8091/get-timeout")
+
+	_, err := cli.Get(uri)
+	//错误处理
 	if err != nil {
-		if resp.IsTimeout() {
-			fmt.Println("timeout")
-			// Output: timeout
-			return
+		// 如果uri 请求超时，则进额外行三次请求，全部超时后返回服务名称
+		for i := 0; i < 3; i++ {
+			time.Sleep(5 * time.Second)
+			_, err := cli.Get(uri)
+			if err != nil {
+				continue
+
+			} else {
+				break
+			}
+
 		}
+		s = "TimeOut"
+		return s
 	}
 
+	return s
 }
